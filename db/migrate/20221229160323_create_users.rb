0c3b459ede604d5379ca2252a6105031352a54ff
Ken Ohata <ohataken@outlook.com>
2022-12-30 01:05:47 +0900

    users.id_hex に unique 制約を追加

--- db/migrate/20221229160323_create_users.rb
+++ db/migrate/20221229160323_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :id_hex
       t.string :name
+      t.string :id_hex, index: { unique: true }
       t.string :crypted_password
       t.string :salt
       t.string :timezone
