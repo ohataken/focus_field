114f8d37ba5ce257b227d4dbb7afe2037659f5fd
Ken Ohata <ohataken@outlook.com>
2022-12-29 23:21:00 +0900

    add sessions#show

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resource :sessions, only: [:new]
+  resource :sessions, only: [:show, :new]
 end
