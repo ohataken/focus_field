9289d0da2be223cc5e9438df9018a72181583efe
Ken Ohata <ohataken@outlook.com>
2022-12-30 01:39:42 +0900

    User の初期化後に id_hex のデフォルト値を設定するように変更した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :id_hex, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true, format: { with: /\A[0-9A-Za-z_]+\z/ }
+
+  after_initialize do |user|
+    user.id_hex ||= SecureRandom.hex(8)
+  end
 end
