ae01c415f5faf8f7c79a5e81cc4847cb74384620
Ken Ohata <ohataken@outlook.com>
2022-12-30 23:15:46 +0900

    fix ApplicationController#current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def current_user
-    @_current_user = User.find_by(id: session[:user_id])
+    @_current_user ||= User.find_by(id: session[:user_id])
   end
 end
