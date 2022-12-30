74a82bc572b32fe224435f2e97522b88c2bb6e7b
Ken Ohata <ohataken@outlook.com>
2022-12-31 02:12:39 +0900

    update

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,19 +1,40 @@
 class CardsController < ApplicationController
   def index
+    @resources = current_user.cards
   end
 
   def new
+    @resource = current_user.cards.build
   end
 
   def create
+    @resource = current_user.cards.build(resource_params)
+
+    if @resource.save
+      redirect_to cards_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+    @resource = current_user.cards.find_by!(id_hex: params[:id])
   end
 
   def update
+    @resource = current_user.cards.find_by!(id_hex: params[:id])
+
+    if @resource.update(resource_params)
+      redirect_to cards_path
+    else
+      render :edit
+    end
+  end
+
+  def resource_params
+    params.require(:card).permit(:name)
   end
 end
