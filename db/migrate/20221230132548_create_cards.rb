8b3b398ff0ca4336890c3d6aea7db7e56f62ead1
Ken Ohata <ohataken@outlook.com>
2022-12-30 22:27:00 +0900

    cards.id_hex に unique 制約を追加した

--- db/migrate/20221230132548_create_cards.rb
+++ db/migrate/20221230132548_create_cards.rb
@@ -1,7 +1,7 @@
 class CreateCards < ActiveRecord::Migration[7.0]
   def change
     create_table :cards do |t|
-      t.string :id_hex
+      t.string :id_hex, index: { unique: true }
       t.string :name
       t.integer :user_id
       t.datetime :finished_at
