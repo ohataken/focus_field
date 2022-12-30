251260d3c8df5bc7a718a5cae5bfbe87ed0e2fdc
Ken Ohata <ohataken@outlook.com>
2022-12-30 23:17:17 +0900

    ApplicationController#logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  def logged_in?
+    current_user.present?
+  end
+
   def current_user
     @_current_user ||= User.find_by(id: session[:user_id])
   end
