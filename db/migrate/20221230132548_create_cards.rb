773ca4061ebc28bb0b641ff87610400d126b40c1
Ken Ohata <ohataken@outlook.com>
2022-12-30 22:26:15 +0900

    rails generate model card

---
+++ db/migrate/20221230132548_create_cards.rb
@@ -0,0 +1,12 @@
+class CreateCards < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cards do |t|
+      t.string :id_hex
+      t.string :name
+      t.integer :user_id
+      t.datetime :finished_at
+
+      t.timestamps
+    end
+  end
+end
