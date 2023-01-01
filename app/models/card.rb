1b0e6d15a82df4552fbc4944a4dfc69593a984d8
Ken Ohata <ohataken@outlook.com>
2023-01-01 21:26:07 +0900

    add a scope Card.unfinished

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,6 +5,7 @@ class Card < ApplicationRecord
 
   scope :order_by_created_at_desc, -> { order(created_at: :desc) }
   scope :finished, -> { where.not(finished_at: nil) }
+  scope :unfinished, -> { where(finished_at: nil) }
 
   after_initialize do |user|
     user.id_hex ||= SecureRandom.hex(16)
