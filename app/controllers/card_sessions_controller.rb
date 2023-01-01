8fbb14079130c4ec676bf86f0abb58bb311b64e6
Ken Ohata <ohataken@outlook.com>
2023-01-02 02:34:31 +0900

    rails generate controller card_sessions

---
+++ app/controllers/card_sessions_controller.rb
@@ -0,0 +1,2 @@
+class CardSessionsController < ApplicationController
+end
---
+++ test/controllers/card_sessions_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CardSessionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
