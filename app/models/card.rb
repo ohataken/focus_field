adcda4306cae46ce4b786c7ae809e44b0aac6b95
Ken Ohata <ohataken@outlook.com>
2023-01-01 21:25:52 +0900

    add a scope Card.order_by_created_at_desc

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,6 +3,8 @@ class Card < ApplicationRecord
   validates :id_hex, presence: true, uniqueness: true
   validates :name, presence: true
 
+  scope :order_by_created_at_desc, -> { order(created_at: :desc) }
+
   after_initialize do |user|
     user.id_hex ||= SecureRandom.hex(16)
   end
