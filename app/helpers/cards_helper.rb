597a47403bfbb5978534f09f9a2ffd5e97669c7c
Ken Ohata <ohataken@outlook.com>
2023-01-04 01:29:30 +0900

    update

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,4 +1,15 @@
 module CardsHelper
+  def card_status_tag card
+    if card.has_ongoing_session?
+      tag.span "doing", class: "tag is-small is-danger"
+    elsif card.finished?
+      tag.span "done", class: "tag is-small is-light is-light"
+    elsif card.unfinished?
+      tag.span "todo", class: "tag is-small is-info is-light"
+    else
+    end
+  end
+
   def card_session_elapsed_time card_session
     if card_session and card_session.ongoing?
       duration = card_session.duration
