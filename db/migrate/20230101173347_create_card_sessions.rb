5847cab19dcb36ff1530f4113cb6abdc0b18a9d3
Ken Ohata <ohataken@outlook.com>
2023-01-05 23:08:08 +0900

    add a index to card_sessions.user_id

--- db/migrate/20230101173347_create_card_sessions.rb
+++ db/migrate/20230101173347_create_card_sessions.rb
@@ -1,7 +1,7 @@
 class CreateCardSessions < ActiveRecord::Migration[7.0]
   def change
     create_table :card_sessions do |t|
-      t.integer :user_id
+      t.integer :user_id, index: true
       t.integer :card_id
       t.datetime :start_at
       t.datetime :end_at
