3b88f3a496e0120ca7d2dc9ffd34c5ebf5fe694f
Ken Ohata <ohataken@outlook.com>
2023-01-07 01:40:53 +0900

    User#crypted_password の validation を設定した

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :id_hex, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true, format: { with: /\A[0-9A-Za-z_]+\z/ }
   validates :password, length: { in: 8..32 }, if: :new_record?
+  validates :crypted_password, presence: true
 
   after_initialize do |user|
     user.id_hex ||= SecureRandom.hex(8)
