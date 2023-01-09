350479be00990f89202b83907fcc044a6879322e
Ken Ohata <ohataken@outlook.com>
2023-01-09 18:16:44 +0900

    add cards.card_generation column

--- db/migrate/20221230132548_create_cards.rb
+++ db/migrate/20221230132548_create_cards.rb
@@ -3,6 +3,7 @@ class CreateCards < ActiveRecord::Migration[7.0]
     create_table :cards do |t|
       t.string :id_hex, index: { unique: true }
       t.string :name
+      t.string :card_generation
       t.integer :user_id, index: true
       t.datetime :finished_at
 
