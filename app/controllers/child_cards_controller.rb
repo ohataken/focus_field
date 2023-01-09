79e79fa073b3b610e706677891a8fa8f9d11de13
Ken Ohata <ohataken@outlook.com>
2023-01-10 00:25:35 +0900

    update child_cards/index

--- app/controllers/child_cards_controller.rb
+++ app/controllers/child_cards_controller.rb
@@ -1,5 +1,7 @@
 class ChildCardsController < ApplicationController
   def index
+    @parent_card = current_user.cards.parent_cards.find_by!(id_hex: params[:card_id])
+    @resources = @parent_card.child_cards
   end
 
   def show
